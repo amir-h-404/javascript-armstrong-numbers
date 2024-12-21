9538aa9f9f42ccb22b0741d99b42e0a3dcedcc08
Amirhossein Emadi <emadi.henker404@gmail.com>
2024-12-21 09:24:28 +0330

    Enhance input validation by checking for non-empty values and using safe integer checks

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function calculateArmStrong() {
   const inputOfUser = document.querySelector("input[name=inputOfUser]");
-  console.log(checkNumberIsInteger(inputOfUser.value));
+  console.log(checkIsNotEmpty(inputOfUser.value));
 }
 
 // check user input is an Integer number:
 function checkNumberIsInteger(input) {
-  if (!Number.isInteger(Number(input))) return false;
+  if (!Number.isSafeInteger(Number(input))) return false;
+  return true;
+}
+
+// check user input is not empty:
+function checkIsNotEmpty(input) {
+  if (input.length === 0) return false;
   return true;
 }
